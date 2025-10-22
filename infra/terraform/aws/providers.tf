d7f8b003dce9702b20a5596d70a7bc5f024c3ef4
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-10-21 21:57:02 -0500

    feat: add elements to deploy

---
+++ infra/terraform/aws/providers.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project" {
+  type    = string
+  default = "auth"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "tf_backend_bucket" {
+  type        = string
+  description = "S3 bucket for terraform remote state (same bucket used for both shared and microservice state)"
+}
+
+variable "shared_state_key" {
+  type        = string
+  default     = "shared/terraform.tfstate"
+  description = "S3 key where shared infrastructure state is stored"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
